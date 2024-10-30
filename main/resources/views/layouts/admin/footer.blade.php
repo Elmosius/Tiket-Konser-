0073ef4972b60069e84dc62a391ea00bc3137411
Elmosius <2272008@maranatha.ac.id>
2024-10-30 11:39:42 +0700

    created template dashboard

---
+++ main/resources/views/layouts/admin/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="py-6 px-6 text-center">
+        <p class="mb-0 fs-4">Copyright 2024 YukNonton.com</p>
+    </div>
+</footer>
---
+++ main/resources/views/layouts/admin/master.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+
+{{-- Head --}}
+@include('layouts.admin.head')
+{{-- End Head --}}
+
+
+<body class="d-flex flex-column min-vh-100">
+    <!--  Body Wrapper -->
+    <div class="page-wrapper flex-grow-1" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6"
+        data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
+        <!-- Sidebar Start -->
+        @include('layouts.admin.sidebar')
+        <!--  Sidebar End -->
+
+        <div class="body-wrapper flex-grow-1">
+            <!--  Header Start -->
+            @include('layouts.admin.header')
+            <!--  Header End -->
+            <div class="container-fluid flex-grow-1">
+                <div class="row flex-grow-1">
+                    @yield('isi-konten-dashboard')
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Footer --}}
+    @include('layouts.admin.footer')
+    {{-- End Footer --}}
+
+    <script src="{{ asset('../assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('../assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('../assets/libs/simplebar/dist/simplebar.js') }}"></script>
+    <script src="{{ asset('../assets/js/sidebarmenu.js') }}"></script>
+    <script src="{{ asset('../assets/js/app.min.js') }}"></script>
+    <script src="{{ asset('../assets/js/dashboard.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+</body>
+
+</html>
